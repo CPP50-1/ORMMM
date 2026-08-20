a4cf091cda00be692688f1e11e2e2829b549f5e3
fexghadi <meydora@proton.me>
2026-08-20 14:40:22 +0200

    Fixed dot mistake and added field types

--- fields.py
+++ fields.py
@@ -16,7 +16,7 @@ class Field:
     def __set__(self, instance, value):
         if self.required and value is None:
             raise ValueError(f"{self.name} is mandatory.")
-        instance.__dict__.[self.name] = value
+        instance.__dict__[self.name] = value
 
     def __repr__(self):
         class_name = self.__class__.__name__
@@ -25,3 +25,45 @@ class Field:
             attributes.append(f"{key}={value!r}")
         attr_string = ", ".join(attributes)
         return f"<{class_name} {attr_string}>"
+
+
+class CharField(Field):
+
+    def __init__(self, max_length: int = 50, **kwargs):
+        super().__init__(f"VARCHAR({max_length})", **kwargs)
+
+
+class TextField(Field):
+
+    def __init__(self, **kwargs):
+        super().__init__("TEXT", **kwargs)
+
+
+class BoolField(Field):
+
+    def __init__(self, **kwargs):
+        super().__init__("BOOLEAN", **kwargs)
+
+
+class IntField(Field):
+
+    def __init__(self, **kwargs):
+        super().__init__("INTEGER", **kwargs)
+
+
+class SmallIntField(Field):
+
+    def __init__(self, **kwargs):
+        super().__init__("SMALLINT", **kwargs)
+
+
+class BigIntField(Field):
+
+    def __init__(self, **kwargs):
+        super().__init__("BIGINT", **kwargs)
+
+
+class DecimalField(Field):
+
+    def __init__(self, **kwargs):
+        super().__init__("DECIMAL", **kwargs)
